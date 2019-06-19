9b3051b8d64582a692af7340b3d5ea15840cd066
Rajeev5155 <41577701+Rajeev5155@users.noreply.github.com>
2019-06-19 23:01:21 +0530

    Update random.py

--- random.py
+++ random.py
@@ -1,7 +1,7 @@
 import random
 chars='abcdefghABCDEFGH'
 length=int(input("Enter the length of the password:\n"))
-
+cnt=int(input("Enter the number of passwords you want?"))
 for p in range(cnt):
     pas=' '
     for c in range(length):
